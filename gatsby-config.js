fcc097fc05838882fa31d5b310c64f75d0475434
Francis <git@francisleigh.co.uk>
2019-05-17 13:04:44 +0100

    color change

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `luckydragonhairs`,
         short_name: `LDH`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F4511E`,
+        theme_color: `#F4511E`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#F4511E`,
       marginBottom: `1.45rem`,
     }}
   >
