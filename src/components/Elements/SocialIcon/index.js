0ddea1cb4976467050119a2f3c295e83f13ee4ab
FrancisLeigh <git@francisleigh.co.uk>
2019-05-27 18:41:18 +0100

    social icon style

--- src/components/Elements/SocialIcon/index.js
+++ src/components/Elements/SocialIcon/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const SocialIcon = ({ height = '50px', width = '50px', icon = void 0, link = '#' }) => (
+const SocialIcon = ({ height = '30px', width = '30px', icon = void 0, link = '#' }) => (
   <a href={link} target="_blank" rel="noopener noreferrer">
     <div style={{
       background: `url(${icon}) round`,
       backgroundSize: `cover`,
+      margin: '0.5rem',
       width, height
     }}></div>
   </a>
