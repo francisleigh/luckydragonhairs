33a87bbce1e688cd2e06c58041ef6c76d3dcd7b8
FrancisLeigh <git@francisleigh.co.uk>
2019-06-15 15:12:35 +0100

    social icon fix inline style

--- src/components/Elements/SocialIcon/index.js
+++ src/components/Elements/SocialIcon/index.js
@@ -3,8 +3,10 @@ import React from 'react'
 const SocialIcon = ({ height = '30px', width = '30px', icon = void 0, link = '#' }) => (
   <a href={link} target="_blank" rel="noopener noreferrer">
     <div style={{
-      background: `url(${icon}) round`,
-      backgroundSize: `cover`,
+      backgroundImage: `url(${icon})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain',
+      backgroundPosition: 'center',
       margin: '0.5rem',
       width, height
     }}></div>
