7d5603bad05d8f7752317912f7b5edb1ff24c928
Francis <git@francisleigh.co.uk>
2019-07-19 17:18:24 +0100

    button styling

--- src/components/Elements/Button/index.js
+++ src/components/Elements/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import button_styles from './button.module.scss'
 const Button = ({text = ``, link = ``, target = `_blank`}) => (
-  <a href={link} target={target} rel="noopener noreferrer" className={button_styles.button}>{text}</a>
+  <a href={link} target={target} rel="noopener noreferrer" className={button_styles.button__moneybox}>{text}</a>
 )
 
 export default Button
