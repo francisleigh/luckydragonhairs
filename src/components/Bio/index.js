3c8fee6690ff3eecfdcf2650669e2a5a8fdc3f2e
Francis <git@francisleigh.co.uk>
2019-07-19 17:18:34 +0100

    bio_text

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -5,10 +5,11 @@ import Container from '../Elements/Container'
 const Bio = () => (
   <Container direction="column" className={container_styles.bio_container}>
     <Container direction="column" padding="1rem" className={container_styles.bio}>
-      <p><strong>Lucky Dragon</strong> is a one-chair barbershop in Stoke Newington that specialises in short haircuts for all ages and all genders.</p>
-      <p>Trained at Toni & Guy, Marshal Darling spent four years working in two of London’s busiest, high-end barbershops before founding Lucky Dragon in 2019.</p>
-      <p>Marshal’s mission is to encourage his clients to take time out of their busy London lives and restore their wellbeing, starting with their hairs and a hot towel.</p>
-      <p><i>“From his attentive cuts to his nourishing conversation and superb record collection, Marshal conjures a replenishing, holistic experience at Lucky Dragon that is second to none.”</i></p>
+      <p className={container_styles.bio__text}><strong>Lucky Dragon</strong> is a one-chair barbershop in Stoke Newington that specialises in short haircuts for all ages and all genders.</p>
+      <p className={container_styles.bio__text}>Trained at Toni & Guy, Marshal Darling spent four years working in two of London’s busiest, high-end barbershops before founding Lucky Dragon in 2019.</p>
+      <p className={container_styles.bio__text}>Marshal’s mission is to encourage his clients to take time out of their busy London lives and restore their wellbeing, starting with their hairs and a hot towel.</p>
+      <p className={container_styles.bio__text}><i>“From his attentive cuts to his nourishing conversation and superb record collection, Marshal conjures a replenishing, holistic experience at Lucky Dragon that is second to none.”</i></p>
+      <p className={container_styles.bio__text}>Joseph Bond</p>
 
       <Container className={container_styles.price_container} padding="0" width="600px" height="50px" style={{justifyContent: 'space-evenly', alignItems: 'center'}}>
         <p className={container_styles.price}>60min haircut = <strong>£45</strong></p>
