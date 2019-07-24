7b715e7817f9199f7f2c8987fbca1cedee76aabe
Francis <git@francisleigh.co.uk>
2019-07-24 13:05:56 +0100

    remove `, high-end` from copy

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -6,7 +6,7 @@ const Bio = () => (
   <Container direction="column" className={container_styles.bio_container}>
     <Container direction="column" padding="1rem" className={container_styles.bio}>
       <p className={container_styles.bio__text}><strong>Lucky Dragon</strong> is a one-chair barbershop in Stoke Newington that specialises in short haircuts for all ages and all genders.</p>
-      <p className={container_styles.bio__text}>Trained at Toni & Guy, Marshal Darling spent four years working in two of London’s busiest, high-end barbershops before founding Lucky Dragon in 2019.</p>
+      <p className={container_styles.bio__text}>Trained at Toni & Guy, Marshal Darling spent four years working in two of London’s busiest barbershops before founding Lucky Dragon in 2019.</p>
       <p className={container_styles.bio__text}>Marshal’s mission is to encourage his clients to take time out of their busy London lives and restore their wellbeing, starting with their hairs and a hot towel.</p>
       <p className={container_styles.bio__text}><i>“From his attentive cuts to his nourishing conversation and superb record collection, Marshal conjures a replenishing, holistic experience at Lucky Dragon that is second to none.”</i></p>
       <p className={container_styles.bio__text}>Joseph Bond</p>
